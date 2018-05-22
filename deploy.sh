07c7ccce5968f24ae37a0a0d74509e925a9be783
Pan <panfree23@gmail.com>
2018-05-23 00:17:31 +0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:PanJiaChen//awesome-bookmarks.git master:gh-pages
+git push -f git@github.com:PanJiaChen/awesome-bookmarks.git master:gh-pages
 
 cd -
