178a6fe0a5bdced1d18a7faa07b5005c8d051507
Pan <panfree23@gmail.com>
2018-05-23 13:07:07 +0800

    test2

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://${access_token}@github.com:PanJiaChen/awesome-bookmarks.git master:gh-pages
+git push -f "https://${access_token}@github.com:PanJiaChen/awesome-bookmarks.git" master:gh-pages
 
 cd -
