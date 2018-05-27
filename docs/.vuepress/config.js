b10b9e74d433f85e1ed897933b3773bd5df12200
Pan <panfree23@gmail.com>
2018-05-27 16:18:52 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  title: 'awesome-bookmarks',
-  description: 'coming soon',
-  base:'/awesome-bookmarks/',
+  title: "awesome-bookmarks",
+  description: "coming soon",
+  base: "/awesome-bookmarks/",
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
-      { text: '库', link: '/repository/' },
-      { text: '文章', link: '/article/' },
+      { text: "首页", link: "/" },
+      { text: "库", link: "/repository/" },
+      { text: "文章", link: "/article/" },
       // { text: '工具', link: 'https://google.com' },
-      { text: '网站', link: '/website/' },
-      { text: '面试', link: '/interview/' }
+      { text: "网站", link: "/website/" },
+      { text: "面试", link: "/interview/" }
     ],
     // sidebar: [
     //   '/',
     //   '/page-a',
     //   ['/page-b', 'Explicit link text']
     // ]
-    lastUpdated: '上次更新',
-    repo: 'PanJiaChen/awesome-bookmarks',
+    lastUpdated: "上次更新",
+    repo: "PanJiaChen/awesome-bookmarks",
     editLinks: true,
-    docsDir: 'docs',
-    editLinkText: '在 GitHub 上编辑此页'
+    docsDir: "docs",
+    editLinkText: "在 GitHub 上编辑此页"
   },
   configureWebpack: {
     resolve: {
       alias: {
-        '@public': './public'
+        "@public": "./public"
       }
     }
-  }
-}
+  },
+  ga: "UA-109340118-1"
+};
